b41177b9ce1302ecc145a52b4c4450303368da53
ancilcleetus <ancilcleetus.work@gmail.com>
2023-02-15 18:11:19 +0530

    Day 01 Part2 added

--- Advent-of-Code/AoC-2017/day_01_inverse_captcha.py
+++ Advent-of-Code/AoC-2017/day_01_inverse_captcha.py
@@ -2,26 +2,6 @@
 
 """
 https://adventofcode.com/2017/day/1
---- Day 1: Inverse Captcha ---
-The night before Christmas, one of Santa's Elves calls you in a panic. "The printer's broken! We can't print the Naughty or Nice List!" By the time you make it to sub-basement 17, there are only a few minutes until midnight. "We have a big problem," she says; "there must be almost fifty bugs in this system, but nothing else can print The List. Stand in this square, quick! There's no time to explain; if you can convince them to pay you in stars, you'll be able to--" She pulls a lever and the world goes blurry.
-
-When your eyes can focus again, everything seems a lot more pixelated than before. She must have sent you inside the computer! You check the system clock: 25 milliseconds until midnight. With that much time, you should be able to collect all fifty stars by December 25th.
-
-Collect stars by solving puzzles. Two puzzles will be made available on each day millisecond in the Advent calendar; the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
-
-You're standing in a room with "digitization quarantine" written in LEDs along one wall. The only door is locked, but it includes a small interface. "Restricted Area - Strictly No Digitized Users Allowed."
-
-It goes on to explain that you may only leave by solving a captcha to prove you're not a human. Apparently, you only get one millisecond to solve the captcha: too fast for a normal human, but it feels like hours to you.
-
-The captcha requires you to review a sequence of digits (your puzzle input) and find the sum of all digits that match the next digit in the list. The list is circular, so the digit after the last digit is the first digit in the list.
-
-For example:
-
-1122 produces a sum of 3 (1 + 2) because the first digit (1) matches the second digit and the third digit (2) matches the fourth digit.
-1111 produces 4 because each digit (all 1) matches the next.
-1234 produces 0 because no digit matches the next.
-91212129 produces 9 because the only digit that matches the next one is the last digit, 9.
-What is the solution to your captcha?
 """
 
 # Standard Library imports
@@ -33,7 +13,7 @@ What is the solution to your captcha?
 
 # add your task specific functions here
 
-def get_inverse_captcha(digit_sequence: str) -> int:
+def get_inverse_captcha_part1(digit_sequence: str) -> int:
     result = 0
 
     for current_digit, next_digit in zip(digit_sequence[:-1], digit_sequence[1:]):
@@ -45,17 +25,50 @@ def get_inverse_captcha(digit_sequence: str) -> int:
 
     return result
 
+assert get_inverse_captcha_part1("1122") == 3
+assert get_inverse_captcha_part1("1111") == 4
+assert get_inverse_captcha_part1("1234") == 0
+assert get_inverse_captcha_part1("91212129") == 9
 
-assert get_inverse_captcha("1122") == 3
-assert get_inverse_captcha("1111") == 4
-assert get_inverse_captcha("1234") == 0
-assert get_inverse_captcha("91212129") == 9
+
+def get_inverse_captcha_part2_01(digit_sequence: str) -> int:
+    seq_length = len(digit_sequence)
+    step = seq_length // 2
+    result = 0
+
+    for current_idx in range(0, seq_length):
+        next_idx = (current_idx + step) % seq_length
+        if digit_sequence[current_idx] == digit_sequence[next_idx]:
+            result += int(digit_sequence[current_idx])
+
+    return result
+
+def get_inverse_captcha_part2_final(digit_sequence: str) -> int:
+    seq_length = len(digit_sequence)
+    step = seq_length // 2
+    result = 0
+    
+    for current_digit, next_digit in zip(digit_sequence, digit_sequence[step:]):
+        if current_digit == next_digit:
+            result += 2 * int(current_digit)
+            
+    return result
+
+assert get_inverse_captcha_part2_final("1212") == 6
+assert get_inverse_captcha_part2_final("1221") == 0
+assert get_inverse_captcha_part2_final("123425") == 4
+assert get_inverse_captcha_part2_final("123123") == 12
+assert get_inverse_captcha_part2_final("12131415") == 4
 
 PUZZLE_INPUT = "111831362354551173134957758417849716877188716338227121869992652972154651632296676464285261171625892888598738721925357479249486886375279741651224686642647267979445939836673253446489428761486828844713816198414852769942459766921928735591892723619845983117283575762694758223956262583556675379533479458964152461973321432768858165818549484229241869657725166769662249574889435227698271439423511175653875622976121749344756734658248245212273242115488961818719828258936653236351924292251821352389471971641957941593141159982696396228218461855752555358856127582128823657548151545741663495182446281491763249374581774426225822474112338745629194213976328762985884127324443984163571711941113986826168921187567861288268744663142867866165546795621466134333541274633769865956692539151971953651886381195877638919355216642731848659649263217258599456646635412623461138792945854536154976732167439355548965778313264824237176152196614333748919711422188148687299757751955297978137561935963366682742334867854892581388263132968999722366495346854828316842352829827989419393594846893842746149235681921951476132585199265366836257322121681471877187847219712325933714149151568922456111149524629995933156924418468567649494728828858254296824372929211977446729691143995333874752448315632185286348657293395339475256796591968717487615896959976413637422536563273537972841783386358764761364989261322293887361558128521915542454126546182855197637753115352541578972298715522386683914777967729562229395936593272269661295295223113186683594678533511783187422193626234573849881185849626389774394351115527451886962844431947188429195191724662982411619815811652741733744864411666766133951954595344837179635668177845937578575117168875754181523584442699384167111317875138179567939174589917894597492816476662186746837552978671142265114426813792549412632291424594239391853358914643327549192165466628737614581458189732579814919468795493415762517372227862614224911844744711698557324454211123571327224554259626961741919243229688684838813912553397698937237114287944446722919198743189848428399356842626198635297851274879128322358195585284984366515428245928111112613638341345371"
 
 if __name__ == "__main__":
-    result =  get_inverse_captcha(PUZZLE_INPUT)
-    print(f"result = {result}")
-    assert result == 1044
+    result1 =  get_inverse_captcha_part1(PUZZLE_INPUT)
+    print(f"result1 = {result1}")
+    assert result1 == 1044
+
+    result2 =  get_inverse_captcha_part2_final(PUZZLE_INPUT)
+    print(f"result2 = {result2}")
+    assert result2 == 1054
 
     print("End of main()")
